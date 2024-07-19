c972224881afe95833a263556d538887044970d4
captan2529 <captan2529@gmail.com>
2024-07-19 10:54:34 +0700

    first commit

---
+++ src/app/getapp/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import GetApp from '../GetApp'
+
+export default function page() {
+  return (
+    <div>
+     <GetApp/>
+    </div>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Footer from './components/footer'
+import Hero from './Hero'
+
+export default function Home() {
+  return (
+    <div>
+    <Hero/>
+    </div>
+  )
+}
